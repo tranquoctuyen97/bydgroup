06afd9406e7a8def68b888a726e02bd4f1bd663b
Tuyen Tran <tuyen.tq@Tuyens-MacBook-Pro.local>
2026-04-24 14:16:59 +0700

    dfdfi

---
+++ src/lib/media/imageSizes.ts
@@ -0,0 +1,17 @@
+export const PAYLOAD_IMAGE_SIZES = [
+  {
+    name: 'thumbnail',
+    width: 320,
+    height: 320,
+  },
+  {
+    name: 'card',
+    width: 640,
+    height: 480,
+  },
+  {
+    name: 'hero',
+    width: 1600,
+    height: 900,
+  },
+]
